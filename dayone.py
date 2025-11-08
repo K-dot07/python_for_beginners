2db5341a5433b0be0c3eef06456ce9aaaff5602d
K-dot07 <kalaiselvandot@gmail.com>
2025-11-08 10:58:29 +0530

    dayone.py is added

--- dayone.py
+++ dayone.py
@@ -1,2 +1,8 @@
 print("hello world")
 print("Kalai")
+# adding a two number and print it
+
+num1 = int (input("Enter the first number :"))# getting the input 1
+num2 = int (input("Enter the second number :"))# getting the input 2
+adding = num1 + num2    #adding the num1+num2
+print("the sum of two number is:", adding)   #display the result 
